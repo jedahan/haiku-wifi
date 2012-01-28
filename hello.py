bf44aeb43b860513e90b7d32288f64e44a0944ea
Jonathan Dahan <jonathan@jedahan.com>
2012-01-27 20:00:26 -0500

    ahahhaha

--- hello.py
+++ hello.py
@@ -7,15 +7,17 @@ import datetime
 @app.route('/say/<line1>/<line2>/<line3>')
 def say_three(line1,line2,line3):
   timestamp = datetime.datetime.now().strftime("%y/%m/%d %H:%M")
-  message = "[%s] %s" % (timestamp, something)
-  os.system("echo %s >> history.log" % message)
-  os.system("tail -n1 history.log")
-  os.system("uci set wireless.@wifi-iface[3].ssid='-1 %s'" % something)
-  os.system("uci commit wireless")
-  os.system("ifup wan")
-  os.system("wifi")
+  lines = [line1, line2, line3]
 
-  return "You said %s at %s" % (something, timestamp)
+  for i in range(len(lines))
+    os.system("echo [%s] %s >> history.log" % (timestamp, lines[i]))
+    os.system("tail -n1 history.log")
+    os.system("uci set wireless.@wifi-iface[%d].ssid='-%d %s'" % (i,i,lines[i]))
+    os.system("uci commit wireless")
+    os.system("ifup wan")
+    os.system("wifi")
+
+  return "At %s, you said %s, %s, %s" % (timestamp, lines[1], lines[2], lines[3])
 
 @app.route('/say/<line1>/<line2>')
 def say_two(line1,line2):
