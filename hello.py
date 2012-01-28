6b52cd9bae4676151387f57045f8de7d2a574848
Jonathan Dahan <jonathan@jedahan.com>
2012-01-28 05:00:42 -0500

    only one ifup/wifi, use spaces instead of -#

--- hello.py
+++ hello.py
@@ -8,14 +8,16 @@ import datetime
 def say_three(line1,line2,line3):
   timestamp = datetime.datetime.now().strftime("%y/%m/%d %H:%M")
   lines = [line1, line2, line3]
+  spaces = ['  ', ' ', '']
 
   for i in range(len(lines))
     os.system("echo [%s] %s >> history.log" % (timestamp, lines[i]))
     os.system("tail -n1 history.log")
-    os.system("uci set wireless.@wifi-iface[%d].ssid='-%d %s'" % (i,i,lines[i]))
-    os.system("uci commit wireless")
-    os.system("ifup wan")
-    os.system("wifi")
+    os.system("uci set wireless.@wifi-iface[%d].ssid='%s %s'" % (i+1,spaces[i],lines[i]))
+
+  os.system("uci commit wireless")
+  os.system("ifup wan")
+  os.system("wifi")
 
   return "At %s, you said %s, %s, %s" % (timestamp, lines[1], lines[2], lines[3])
 
