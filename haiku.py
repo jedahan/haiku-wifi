88abf399138e431e1ff0625a3ba405179d4c3fef
Toby Schachman <tobyschachman@gmail.com>
2012-01-30 21:37:30 -0500

    replaced haiku.py with hello.py from the router

--- haiku.py
+++ haiku.py
@@ -1,31 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 app = Flask(__name__)
 
 import os
 import datetime
 
-# Set this offset to the first radio you want to override
-radio_offset = 1
-
 @app.route('/say', methods=['GET', 'POST'])
-def say_three(line1,line2,line3):
+def say():
   timestamp = datetime.datetime.now().strftime("%y/%m/%d %H:%M")
   if request.method == 'POST':
     lines = [request.form['line1'], request.form['line2'], request.form['line3']]
   else:
-    lines = [request.args.get('line1', ''), request.args.get('line2', ''), request.args.get('line3', '')]
-  spaces = ['  ', ' ', '']
+    lines = [ request.args.get('line1', ''), request.args.get('line2', ''), request.args.get('line3', '') ]
 
+  print lines
+  spaces = ['-1', '-2', '-3']
+                              
   for i in range(len(lines)):
+    lines[i] = lines[i].replace("'", "")
     os.system("echo [%s] %s >> history.log" % (timestamp, lines[i]))
     os.system("tail -n1 history.log")
-    os.system("uci set wireless.@wifi-iface[%d].ssid='-%d %s'" % (i+radio_offset,i,lines[i]))
-
+    os.system("uci set wireless.@wifi-iface[%d].ssid='%s %s'" % (i,spaces[i],lines[i]))
   os.system("uci commit wireless")
   os.system("ifup wan")
   os.system("wifi")
+                              
+  return 'success'                               
 
-  return "At %s, you said %s, %s, %s" % (timestamp, lines[1], lines[2], lines[3])
 
+@app.route('/')
+def home():
+  return redirect("/static/index.html")
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+  return redirect("/static/index.html")
+  
 if __name__ == "__main__":
-  app.run(port=8888,host='0.0.0.0',debug=True)
+  app.run(port=80,host='0.0.0.0',debug=True)
